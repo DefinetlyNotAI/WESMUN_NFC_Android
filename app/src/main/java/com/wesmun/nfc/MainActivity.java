94ee7d41ff898a4491ad74988d81fc33aded82cb
Shahm Najeeb <Nirt_12023@outlook.com>
2025-11-21 23:12:30 +0400

    Inject global JS variable for Android APK in MainActivity

--- app/src/main/java/com/wesmun/nfc/MainActivity.java
+++ app/src/main/java/com/wesmun/nfc/MainActivity.java
@@ -1,5 +1,14 @@
 package com.wesmun.nfc;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inject a global JS variable into the WebView
+        this.bridge.getWebView().evaluateJavascript("window.IS_ANDROID_APK = true;", null);
+    }
+}
